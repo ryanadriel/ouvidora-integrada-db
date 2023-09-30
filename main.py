81d72851bc596915dddf9e1760f05fd13dc7ee4f
Adriel Ryan <adrielryan.tj@hotmail.com>
2023-09-30 18:16:16 -0300

    Atualizando Visualização das manifestações

--- main.py
+++ main.py
@@ -6,15 +6,22 @@ while opcao != 5:
   
   opcao = int(input("Digite um opção: "))
   
+  #Listagem de manifestações
   if opcao == 1:
-    #Listagem de manifestações
-    if len(manifestacoes) != 0:
-      print("Listagem de Manifestações:")
-      for x in manifestacoes:
+    print("Escolha o tipo de manifestação a exibir: \n 1 - Sugestão \n 2 - Reclamação \n 3 - Elogio")
+    tipoManifestacao = int(input("Digite a opção: "))
+    
+    if tipoManifestacao == 1:
+      for x in manifestacoes[0]:
+        print("-" + x)
+    elif tipoManifestacao == 2:
+      for x in manifestacoes[1]:
+        print("-" + x)
+    elif tipoManifestacao == 3:
+      for x in manifestacoes[0]:
         print("-" + x)
     else:
-      print("Não possui manifestações no momento!")
-      
+      print("Opção Inválida.")
   elif opcao == 2:
     print("Insira os dados para adicionar uma nova manifestação:")
     print("Escolha o tipo de manifestação: \n 1 - Sugestão \n 2 - Reclamação \n 3 - Elogio")
@@ -22,8 +29,9 @@ while opcao != 5:
     tipoManifestacao = int(input("Digite o tipo da manifestacao: "))
     
     if tipoManifestacao == 1:
-      novaSugestao = input("Digite sua sugestão")
+      novaSugestao = input("Digite sua sugestão: ")
       manifestacoes[0].append(novaSugestao)
+      print()
       print("Manifestação cadastrada com sucesso")
     
     elif tipoManifestacao == 2:
@@ -35,7 +43,12 @@ while opcao != 5:
       manifestacoes[2].append(novoElogio)
       print("Manifestação cadastrada com sucesso")
   elif opcao == 3:
+   ''' print("Escolha o tipo de manifestação a ser removida: \n 1 - Sugestão \n 2 - Reclamação \n 3 - Elogio")
+    tipoManifestacao = int(input("Digite a opção: "))
     
-    if len(manifestacoes) != 0:
-      print("Manifestações:")
+    if tipoManifestacao == 1:
+      print("Digite o opção a ser removida")
+      
+      for manifesto in len(manifestacoes[0]):
+        print(manifesto)'''
       
